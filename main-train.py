645942529997e9bafc98451218059ed9bee9f645
igalts <igalts@gmail.com>
2016-08-10 14:00:36 +0300

    last

--- main-train.py
+++ main-train.py
@@ -19,26 +19,20 @@ _NEPOCH = int(os.environ.get('NEPOCH', '100'))
 _MODEL_FILE = os.environ.get('MODEL_FILE')
 
 def train_with_sgd(model, X_train, y_train, learning_rate=0.005, nepoch=1, evaluate_loss_after=5):
-    # We keep track of the losses so we can plot them later
     losses = []
     num_examples_seen = 0
     for epoch in range(nepoch):
-        # Optionally evaluate the loss
+
         if (epoch % evaluate_loss_after == 0):
             loss = model.calculate_loss(X_train, y_train)
             losses.append((num_examples_seen, loss))
             time = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
-            print "%s: Loss after num_examples_seen=%d epoch=%d: %f" % (time, num_examples_seen, epoch, loss)
-            # Adjust the learning rate if loss increases
+
             if (len(losses) > 1 and losses[-1][1] > losses[-2][1]):
                 learning_rate = learning_rate * 0.5  
-                print "Setting learning rate to %f" % learning_rate
             sys.stdout.flush()
-            # ADDED! Saving model oarameters
             save_model_parameters_theano("./data/rnn-theano-%d-%d-%s.npz" % (model.hidden_dim, model.word_dim, time), model)
-        # For each training example...
         for i in range(len(y_train)):
-            # One SGD step
             model.sgd_step(X_train[i], y_train[i], learning_rate)
             num_examples_seen += 1
 
@@ -47,38 +41,30 @@ unknown_token = "UNKNOWN_TOKEN"
 sentence_start_token = "SENTENCE_START"
 sentence_end_token = "SENTENCE_END"
 
-# Read the data and append SENTENCE_START and SENTENCE_END tokens
-print "Reading CSV file..."
+
 with open('data/input.csv', 'rb') as f:
     reader = csv.reader(f, skipinitialspace=True)
     reader.next()
-    # Split full comments into sentences
     sentences = itertools.chain(*[nltk.sent_tokenize(x[0].decode('utf-8').lower()) for x in reader])
-    # Append SENTENCE_START and SENTENCE_END
     sentences = ["%s %s %s" % (sentence_start_token, x, sentence_end_token) for x in sentences]
 print "Parsed %d sentences." % (len(sentences))
     
-# Tokenize the sentences into words
+
 tokenized_sentences = [nltk.word_tokenize(sent) for sent in sentences]
 
-# Count the word frequencies
 word_freq = nltk.FreqDist(itertools.chain(*tokenized_sentences))
-print "Found %d unique words tokens." % len(word_freq.items())
 
-# Get the most common words and build index_to_word and word_to_index vectors
+
 vocab = word_freq.most_common(vocabulary_size-1)
 index_to_word = [x[0] for x in vocab]
 index_to_word.append(unknown_token)
 word_to_index = dict([(w,i) for i,w in enumerate(index_to_word)])
 
-print "Using vocabulary size %d." % vocabulary_size
-print "The least frequent word in our vocabulary is '%s' and appeared %d times." % (vocab[-1][0], vocab[-1][1])
 
-# Replace all words not in our vocabulary with the unknown token
+
 for i, sent in enumerate(tokenized_sentences):
     tokenized_sentences[i] = [w if w in word_to_index else unknown_token for w in sent]
 
-# Create the training data
 X_train = np.asarray([[word_to_index[w] for w in sent[:-1]] for sent in tokenized_sentences])
 y_train = np.asarray([[word_to_index[w] for w in sent[1:]] for sent in tokenized_sentences])
 
@@ -87,7 +73,6 @@ model = RNNTheano(vocabulary_size, hidden_dim=_HIDDEN_DIM)
 t1 = time.time()
 model.sgd_step(X_train[10], y_train[10], _LEARNING_RATE)
 t2 = time.time()
-print "SGD Step time: %f milliseconds" % ((t2 - t1) * 1000.)
 
 if _MODEL_FILE != None:
     load_model_parameters_theano(_MODEL_FILE, model)
@@ -96,13 +81,10 @@ train_with_sgd(model, X_train, y_train, nepoch=_NEPOCH, learning_rate=_LEARNING_
 
 
 def generate_sentence(model):
-    # We start the sentence with the start token
     new_sentence = [word_to_index[sentence_start_token]]
-    # Repeat until we get an end token
     while not new_sentence[-1] == word_to_index[sentence_end_token]:
         next_word_probs = model.forward_propagation(new_sentence)
         sampled_word = word_to_index[unknown_token]
-        # We don't want to sample unknown words
         while sampled_word == word_to_index[unknown_token]:
             samples = np.random.multinomial(1, next_word_probs[-1])
             sampled_word = np.argmax(samples)
@@ -116,7 +98,6 @@ senten_max_length = 50
 
 for i in range(num_sentences):
     sent = []
-    # We want long sentences, not sentences with one or two words
     while senten_min_length > len(sent) :
         sent = generate_sentence(model)
     print " ".join(sent)
--- theano.py
+++ theano.py
@@ -7,19 +7,15 @@ import operator
 class RNNTheano:
     
     def __init__(self, word_dim, hidden_dim=100, bptt_truncate=4):
-        # Assign instance variables
         self.word_dim = word_dim
         self.hidden_dim = hidden_dim
         self.bptt_truncate = bptt_truncate
-        # Randomly initialize the network parameters
         U = np.random.uniform(-np.sqrt(1./word_dim), np.sqrt(1./word_dim), (hidden_dim, word_dim))
         V = np.random.uniform(-np.sqrt(1./hidden_dim), np.sqrt(1./hidden_dim), (word_dim, hidden_dim))
         W = np.random.uniform(-np.sqrt(1./hidden_dim), np.sqrt(1./hidden_dim), (hidden_dim, hidden_dim))
-        # Theano: Created shared variables
         self.U = theano.shared(name='U', value=U.astype(theano.config.floatX))
         self.V = theano.shared(name='V', value=V.astype(theano.config.floatX))
         self.W = theano.shared(name='W', value=W.astype(theano.config.floatX))      
-        # We store the Theano graph here
         self.theano = {}
         self.__theano_build__()
     
@@ -42,18 +38,15 @@ class RNNTheano:
         prediction = T.argmax(o, axis=1)
         o_error = T.sum(T.nnet.categorical_crossentropy(o, y))
         
-        # Gradients
         dU = T.grad(o_error, U)
         dV = T.grad(o_error, V)
         dW = T.grad(o_error, W)
         
-        # Assign functions
         self.forward_propagation = theano.function([x], o)
         self.predict = theano.function([x], prediction)
         self.ce_error = theano.function([x, y], o_error)
         self.bptt = theano.function([x, y], [dU, dV, dW])
         
-        # SGD
         learning_rate = T.scalar('learning_rate')
         self.sgd_step = theano.function([x,y,learning_rate], [], 
                       updates=[(self.U, self.U - learning_rate * dU),
@@ -64,31 +57,22 @@ class RNNTheano:
         return np.sum([self.ce_error(x,y) for x,y in zip(X,Y)])
     
     def calculate_loss(self, X, Y):
-        # Divide calculate_loss by the number of words
         num_words = np.sum([len(y) for y in Y])
         return self.calculate_total_loss(X,Y)/float(num_words)   
 
 
 def gradient_check_theano(model, x, y, h=0.001, error_threshold=0.01):
-    # Overwrite the bptt attribute. We need to backpropagate all the way to get the correct gradient
     model.bptt_truncate = 1000
-    # Calculate the gradients using backprop
     bptt_gradients = model.bptt(x, y)
-    # List of all parameters we want to chec.
     model_parameters = ['U', 'V', 'W']
-    # Gradient check for each parameter
     for pidx, pname in enumerate(model_parameters):
-        # Get the actual parameter value from the mode, e.g. model.W
         parameter_T = operator.attrgetter(pname)(model)
         parameter = parameter_T.get_value()
         print "Performing gradient check for parameter %s with size %d." % (pname, np.prod(parameter.shape))
-        # Iterate over each element of the parameter matrix, e.g. (0,0), (0,1), ...
         it = np.nditer(parameter, flags=['multi_index'], op_flags=['readwrite'])
         while not it.finished:
             ix = it.multi_index
-            # Save the original value so we can reset it later
             original_value = parameter[ix]
-            # Estimate the gradient using (f(x+h) - f(x-h))/(2*h)
             parameter[ix] = original_value + h
             parameter_T.set_value(parameter)
             gradplus = model.calculate_total_loss([x],[y])
@@ -98,11 +82,8 @@ def gradient_check_theano(model, x, y, h=0.001, error_threshold=0.01):
             estimated_gradient = (gradplus - gradminus)/(2*h)
             parameter[ix] = original_value
             parameter_T.set_value(parameter)
-            # The gradient for this parameter calculated using backpropagation
             backprop_gradient = bptt_gradients[pidx][ix]
-            # calculate The relative error: (|x - y|/(|x| + |y|))
             relative_error = np.abs(backprop_gradient - estimated_gradient)/(np.abs(backprop_gradient) + np.abs(estimated_gradient))
-            # If the error is to large fail the gradient check
             if relative_error > error_threshold:
                 print "Gradient Check ERROR: parameter=%s ix=%s" % (pname, ix)
                 print "+h Loss: %f" % gradplus
